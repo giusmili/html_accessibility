81e5244af2b610437334c61f83a5970efbacba6c
giusmili <giusmili67@gmail.com>
2021-05-12 16:34:41 +0200

    modified date

--- src/portefolio.js
+++ src/portefolio.js
@@ -1,11 +1,24 @@
 document.addEventListener("DOMContentLoaded",()=>{
     console.log(200)
-    let el, main
+    let el, main, date
     main = document.querySelector("body")
     el = document.querySelector(".accessibility-page")
+    /* copy */
+    date = ({
+        myDate : new Date().getFullYear(),
+        target : document.querySelector("footer p")
+       
+    })
+    date.target.innerText += date.myDate;
 
     el.addEventListener("click",()=>{
 
         main.classList.toggle("switch-accessibility")
     })
 })
+
+
+       
+        
+      
+       
